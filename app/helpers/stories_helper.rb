23cfd396f25b591a5e161267dab5adf6e561e011
lelelelemon <jwy@jws-MacBook-Pro.local>
2018-07-13 04:18:59 -0500

    remove costful query

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -8,11 +8,6 @@ module StoriesHelper
       return true
     end
 
-    if Moderation.joins(:story).
-    where("stories.user_id = ? AND moderations.created_at > ?", @user.id,
-    5.days.ago).count > 0
-      return true
-    end
 
     false
   end
